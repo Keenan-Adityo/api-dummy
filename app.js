891ae5c4e0d23242ea675feb9f6be463ca030c5d
dafariski77 <rskdafa@gmail.com>
2023-11-08 11:40:18 +0700

    chore(morga): change logger format

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 const tokopediaRoute = require("./app/tokopedia/routes");
 
-app.use(logger("dev"));
+app.use(logger(":status :method :url - :response-time ms"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
