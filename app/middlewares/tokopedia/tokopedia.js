ae3bbeb930ef46a40121df4b17b094fd75830ca7
dafariski77 <rskdafa@gmail.com>
2023-11-16 14:19:12 +0700

    refactor: change message middleware

--- app/middlewares/tokopedia/tokopedia.js
+++ app/middlewares/tokopedia/tokopedia.js
@@ -22,16 +22,7 @@ const checkUser = async (req, res, next) => {
     });
 
     if (!userToken) {
-      return res.status(400).json({
-        header: {
-          process_time: 0.018328845,
-          messages:
-            "We could not process your request due to malformed request, please check again",
-          reason: "Failed To Initialize Request",
-          error_code: "ORD_API_001",
-        },
-        data: null,
-      });
+      throw new UnauthenticatedError("Invalid token!");
     }
 
     next();
