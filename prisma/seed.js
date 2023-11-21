f981b5dc6a4af6d1acffde215fa08566611d043c
dafariski77 <rskdafa@gmail.com>
2023-11-21 15:53:38 +0700

    feat(shopee): add user schema and seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,3 +1,5 @@
+const { encrypt } = require("../app/utils/encrypt");
+const { generateRandomString } = require("../app/utils/generateRandom");
 const prisma = require("../app/utils/prisma");
 
 async function main() {
@@ -6,6 +8,18 @@ async function main() {
       fsId: 1122,
       accessToken: Buffer.from(`${1212}:rahasia123`).toString("base64"),
     },
+  });
+
+  encrypt("rahasia123").then((hashedPassword) => {
+    const shopeeUser = prisma.shopeeUser.create({
+      data: {
+        partnerId: 555,
+        partnerKey: hashedPassword,
+        accessToken: generateRandomString(25),
+      },
+    });
+
+    return shopeeUser;
   });
 }
 
